6ccbc7b9e96bc7f106f7703303c0b6e467328a84
Juan Maldini <juan.maldini@outlook.com>
2025-09-22 15:25:00 -0300

    Update OfficePlayer iframe and add .rooot CSS class
    Added a new .rooot class in App.css for centering and sizing the container. Refactored OfficePlayer.jsx to use the iframe element directly with height and width attributes, and updated the title and allowfullscreen properties.

--- src/OfficePlayer.jsx
+++ src/OfficePlayer.jsx
@@ -4,12 +4,14 @@ function OfficePlayer() {
   return (
     <div className="rooot">
       <iframe
-        style={{ visibility: 'visible', border: 'none',width: '100%',height: '100%'}}
-        id='iframe_1'
-        src='https://connector.eagle3dstreaming.com/v6/eyJvd25lciI6IlZhbmlzaGluZ1BvaW50M0QiLCJhcHBOYW1lIjoiVk9mZmljZSIsImNvbmZpZ05hbWUiOiJWT2ZmaWNlIn0='
-        title='Office Configurator'
-        allowFullScreen
-      />
+      style={{ visibility: 'visible', border: 'none' }}
+      id='iframe_1'
+      src='https://connector.eagle3dstreaming.com/v6/eyJvd25lciI6IlZhbmlzaGluZ1BvaW50M0QiLCJhcHBOYW1lIjoiVk9mZmljZSIsImNvbmZpZ05hbWUiOiJWT2ZmaWNlIn0='
+      height='100%'
+      width='100%'
+      allowfullscreen
+      title='host'
+      ></iframe>
     </div>
   );
 }
